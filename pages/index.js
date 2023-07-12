21562de9afafa1964dbed2755494966447352e41
Luca Nerlich <luca.nerlich@googlemail.com>
2023-07-12 08:39:13 +0200

    add mastodon verification

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ export default function Index() {
 
     return (
         <Layout>
+            <a rel="me" href="https://mastodon.social/@luca00">Mastodon</a>
             <CustomHead title="EzBudget"
                         description="Add income / expense"/>
             <MainInput/>
