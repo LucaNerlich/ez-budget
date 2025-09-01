a076eb4af03a641951535a57be1b9b8f89d01662
Luca Nerlich <nerlich@adobe.com>
2025-09-01 11:20:30 +0200

    add recurring entries

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,40 +1,40 @@
-export const metadata = {
-  title: 'EzBudget',
-  description: 'Add income / expense',
-  openGraph: {
-    type: 'website',
-    siteName: 'ezbudget.de'
-  },
-  themeColor: '#ffffff'
-};
-
+import {Metadata} from "next";
 import '../src/styles/app.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Script from 'next/script';
 import DataProvider from '../src/providers/DataProvider';
 import BootstrapClient from './BootstrapClient';
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="de">
-      <head>
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="msapplication-config" content="/icons/favicons/browserconfig.xml" />
-      </head>
-      <body>
+export const metadata: Metadata = {
+    title: 'EzBudget',
+    description: 'Add income / expense',
+    openGraph: {
+        type: 'website',
+        siteName: 'ezbudget.de'
+    },
+};
+
+export default function RootLayout({children}: { children: React.ReactNode }) {
+    return (
+        <html lang="de">
+        <head>
+            <meta name="msapplication-TileColor" content="#da532c"/>
+            <meta name="msapplication-config" content="/icons/favicons/browserconfig.xml"/>
+        </head>
+        <body>
         <DataProvider>
-          {children}
+            {children}
         </DataProvider>
-        <BootstrapClient />
+        <BootstrapClient/>
         <Script
-          id="umami"
-          strategy="afterInteractive"
-          src="https://umami-t8kgsg4o4wc4o80wgwwo484c.lucanerlich.com/script.js"
-          data-website-id="cc45fb2f-c85e-4c87-8332-43ef11b3a215"
+            id="umami"
+            strategy="afterInteractive"
+            src="https://umami-t8kgsg4o4wc4o80wgwwo484c.lucanerlich.com/script.js"
+            data-website-id="cc45fb2f-c85e-4c87-8332-43ef11b3a215"
         />
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
 
 
